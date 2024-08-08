182655b342d5d77bfd1a9ad774af9b63c96975ed
AndrewYeo03 <junken03@gmail.com>
2024-08-08 13:24:59 +0800

    Created New Files

---
+++ app/src/main/java/com/example/mobileapplicationassignment/frontEndUi/tools/Image_File_Import.kt
@@ -0,0 +1,18 @@
+package com.example.mobileapplicationassignment.frontEndUi.tools
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.mobileapplicationassignment.ui.theme.MobileApplicationAssignmentTheme
+
+@Composable
+fun ImageFileImport (){
+
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun PrevImageFileImport (){
+    MobileApplicationAssignmentTheme {
+        ImageFileImport()
+    }
+}
---
+++ app/src/main/java/com/example/mobileapplicationassignment/frontEndUi/tools/Text_Recognition.kt
@@ -0,0 +1,18 @@
+package com.example.mobileapplicationassignment.frontEndUi.tools
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.mobileapplicationassignment.ui.theme.MobileApplicationAssignmentTheme
+
+@Composable
+fun TextRecognition (){
+    
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun PrevTextRecognition (){
+    MobileApplicationAssignmentTheme {
+        TextRecognition()
+    }
+}
